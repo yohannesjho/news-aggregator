4f7878042ea877b10bf27dddeff8abf817851b5f
yohannesjho <jhonians@outlook.com>
2024-12-13 11:46:00 +0300

    Add setsources controller

--- controllers/preferenceControllers.js
+++ controllers/preferenceControllers.js
@@ -18,4 +18,16 @@ const setInterests = async (req, res) => {
         res.status(500).json({ error: 'Server error' });
     }
 };
-module.exports = setInterests
+
+const setSources = async (req,res) =>{
+    const {sources} = req.body
+    console.log(sources)
+    try {
+        const result = await pool.query('INSERT INTO user_preferences(user_id,sources) VALUES($1,$2) ON CONFLICT (user_id) DO UPDATE SET sources = $2 RETURNING * ',[req.user.id,sources])
+        console.log(result.rows)
+        res.status(201).json({msg:"sources added successfully",result})
+    } catch (error) {
+        res.status(500).json({msg:"internal error"})
+    }
+}
+module.exports = {setInterests,setSources}
--- routes/preferenceRoutes.js
+++ routes/preferenceRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const setinterests = require('../controllers/preferenceControllers')
+const { setInterests,setSources} = require('../controllers/preferenceControllers')
 const authenticateToken = require('../middlewares/authenticate')
 
-router.post('/interests',authenticateToken, setinterests)
+router.post('/interests',authenticateToken, setInterests)
+router.post('/sources',authenticateToken, setSources)
 
 module.exports = router
