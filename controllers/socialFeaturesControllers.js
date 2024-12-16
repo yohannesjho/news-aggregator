39e7a172ffec2375d0870f53c046b7ed5394f9e2
yohannesjho <jhonians@outlook.com>
2024-12-16 10:27:25 +0300

    Add save article controller

--- controllers/socialFeaturesControllers.js
+++ controllers/socialFeaturesControllers.js
@@ -9,3 +9,13 @@ const shareArticle = async (req,res)=>{
         res.status(500).send({message:"server error"})
     }
 }
+
+const saveArticle = async (req,res) => {
+    const {articleUrl} = req.body
+    try {
+        await pool.query("INSERT INTO saved_article(user_id,article_url) VALUES($1,$2)",[req.user.id,articleUrl])
+        res.status(201).send({message:"article saved successfully"})
+    } catch (error) {
+        res.status(500).send({message:"server error"})
+    }
+}
--- routes/socialFeaturesRoutes.js
+++ routes/socialFeaturesRoutes.js
@@ -3,3 +3,5 @@ const express = require('express')
 const router = express.Router()
 
 router.post('/share',shareArticle)
+
+router.post('/save',saveArticle)
